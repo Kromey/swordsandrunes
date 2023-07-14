b14cda1184240aa9d9d072b3dd683f8e81ff8d1b
Travis Veazey <travisvz@gmail.com>
2023-07-13 22:22:41 -0800

    Fix tunnels sometimes ending in nowhere

--- src/dungeon/tunnel.rs
+++ src/dungeon/tunnel.rs
@@ -1,4 +1,4 @@
-use std::cmp::{min, max};
+use std::{cmp::{min, max}, ops::RangeInclusive};
 
 use itertools::Itertools;
 use rand::{SeedableRng, Rng};
@@ -8,10 +8,10 @@ use crate::tiles::TilePos;
 
 
 pub fn simple_tunnel(start: TilePos, end: TilePos) -> impl Iterator<Item = TilePos> {
-    let x1 = min(start.x, end.x);
-    let x2 = max(start.x, end.x);
-    let y1 = min(start.y, end.y);
-    let y2 = max(start.y, end.y);
+    let x1 = start.x;
+    let x2 = end.x;
+    let y1 = start.y;
+    let y2 = end.y;
 
     let (corner_x, corner_y) = if Xoshiro512StarStar::from_entropy().gen_bool(0.5) {
         (x2, y1)
@@ -19,7 +19,14 @@ pub fn simple_tunnel(start: TilePos, end: TilePos) -> impl Iterator<Item = TileP
         (x1, y2)
     };
 
-    (x1..=corner_x).cartesian_product(y1..=corner_y)
-        .chain((corner_x..=x2).cartesian_product(corner_y..=y2))
+    make_range(x1, corner_x).cartesian_product(make_range(y1, corner_y))
+        .chain(make_range(corner_x, x2).cartesian_product(make_range(corner_y, y2)))
         .map(TilePos::from)
+}
+
+fn make_range(from: u32, to: u32) -> RangeInclusive<u32> {
+    let a = min(from, to);
+    let b = max(from, to);
+
+    a..=b
 }
